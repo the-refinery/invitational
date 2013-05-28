7b24b4171cc953f202c91d5c0cac513fb342409e
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-05-28 17:46:19 -0400

    Remove the old test stuff

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '021ef1f847fc4a34f2dccf0aee840ced10d9f99ed837e0610a85a048bd37855238fbc276e244c5e71c45ccc077ce6440137a5986cb7b4d1cc5f2059290d5e8ac'
--- test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount Invitational::Engine => "/invitational"
-end
