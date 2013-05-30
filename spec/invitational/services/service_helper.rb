0185e9066ac9c2bfcb9a4fb19a26a9b1661f8b52
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-05-30 19:54:57 -0400

    One file I missed in the last commit

--- spec/invitational/services/service_helper.rb
+++ spec/invitational/services/service_helper.rb
@@ -13,7 +13,7 @@ def setup_entity name
 end
 
 def invite_user user, entity, role
-  invitation = Invitable::Invitation.new(email: user.email, invitable: entity, role: role, user: user)
+  invitation = Invitational::Invitation.new(email: user.email, invitable: entity, role: role, user: user)
   invitation.save
 
   invitation
