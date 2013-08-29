06d3882a1352d338bac0e0e791ead34211970b61
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-08-29 15:32:13 -0400

    Cleanup on this spec

--- spec/invitational/configuration/role_definition_spec.rb
+++ spec/invitational/configuration/role_definition_spec.rb
@@ -3,15 +3,12 @@ require 'spec_helper'
 describe Invitational do
 
   context "configured roles" do
-    When {Invitational.define_roles :none, :admin}
-
     Then {Invitational::Role[:none].should == 0}
-    And  {Invitational::Role[:admin].should == 1}
+    And  {Invitational::Role[:user].should == 1}
+    And  {Invitational::Role[:admin].should == 2}
   end
 
   context "uberadmin role" do
-    When {Invitational.define_roles :none, :admin}
-
     Then {Invitational::Role[:uberadmin].should == -1}
   end
 
