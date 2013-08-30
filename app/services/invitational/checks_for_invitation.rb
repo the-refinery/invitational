dfbec0442171b7a825c10d5461c47072d704da84
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-08-30 15:19:22 -0400

    ChecksForInvitation now supports an array of roles

--- app/services/invitational/checks_for_invitation.rb
+++ app/services/invitational/checks_for_invitation.rb
@@ -1,8 +1,8 @@
 module Invitational
   class ChecksForInvitation
 
-    def self.for user, invitable, role=nil
-      self.uber_admin?(user) || self.specific_invite?(user, invitable, role)
+    def self.for user, invitable, roles=nil
+      self.uber_admin?(user) || self.specific_invite?(user, invitable, roles)
     end
 
 private 
@@ -11,15 +11,24 @@ private
       user.invitations.uber_admin.count == 1
     end
 
-    def self.specific_invite? user, invitable, role
+    def self.specific_invite? user, invitable, roles
       invites = user.invitations.for_invitable(invitable.class.name, invitable.id)
 
       if invites.count > 0
-        unless role.nil?
-          invites.first.role == Invitational::Role[role]
+        unless roles.nil?
+          self.role_check invites.first, roles
         else
           true
         end
+      end
+    end
+
+    def self.role_check invitation, roles
+      if roles.respond_to? :map
+        role_numbers = roles.map {|role| Invitational::Role[role]}
+        role_numbers.include? invitation.role
+      else
+        invitation.role == Invitational::Role[roles]
       end
     end
 
--- spec/invitational/services/checks_for_invitation_spec.rb
+++ spec/invitational/services/checks_for_invitation_spec.rb
@@ -58,6 +58,32 @@ describe Invitational::ChecksForInvitation do
 
       Then {result.should be_true}
     end
+  end
+
+  context "checking for any of an array of roles" do
+    context "when invited to one of the roles" do
+      When(:result) { Invitational::ChecksForInvitation.for user1, entity, [:user, :admin] }
+
+      Then {result.should be_true}
+    end
+
+    context "when invited in another role" do
+      When(:result) { Invitational::ChecksForInvitation.for user1, entity, [:none, :admin] }
+
+      Then {result.should_not be_true}
+    end
+
+    context "when not invited" do
+      When(:result) { Invitational::ChecksForInvitation.for user2, entity, [:user, :admin] }
+
+      Then {result.should_not be_true}
+    end
+
+    context "when uber admin" do
+      When(:result) { Invitational::ChecksForInvitation.for user3, entity, [:user, :admin] }
+
+      Then {result.should be_true}
+    end
 
   end
 
