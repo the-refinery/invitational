d8ce353fee1afa418966b6a45e788ac2e9f994fb
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-09-23 14:58:35 -0400

    A little bit more robust of tests showing different entities

--- spec/invitational/models/ability_spec.rb
+++ spec/invitational/models/ability_spec.rb
@@ -8,34 +8,37 @@ describe Ability do
   Given(:user2) { setup_user "test2@d-i.co" }
   Given(:user3) { setup_user "test3@d-i.co" }
 
-  Given(:entity) { setup_entity "Test entity"}
+  Given(:entity1) { setup_entity "Test entity 1"}
+  Given(:entity2) { setup_entity "Test entity 1"}
 
-  Given {invite_user user1, entity, :user}
-  Given {invite_user user2, entity, :admin}
+  Given {invite_user user1, entity1, :user}
+  Given {invite_user user2, entity2, :admin}
   Given {invite_uber_admin user3}
 
   context "User" do
     Given (:i) { Ability.new(user1) }
     When (:role) {:user}
 
-    Then { i.should be_able_to(:read, entity) }
-    Then { i.should_not be_able_to(:manage, entity) }
+    Then { i.should be_able_to(:read, entity1) }
+    And { i.should_not be_able_to(:manage, entity1) }
+    And { i.should_not be_able_to(:read, entity2) }
   end
 
   context "Admin" do
     Given (:i) { Ability.new(user2) }
     When (:role) {:admin}
 
-    Then { i.should be_able_to(:read, entity) }
-    Then { i.should be_able_to(:manage, entity) }
+    Then { i.should be_able_to(:read, entity2) }
+    And { i.should be_able_to(:manage, entity2) }
+    And { i.should_not be_able_to(:read, entity1) }
   end
 
   context "Uber Admin" do
     Given (:i) { Ability.new(user3) }
     When (:role) {:uber_admin}
 
-    Then { i.should be_able_to(:read, entity) }
-    Then { i.should be_able_to(:manage, entity) }
+    Then { i.should be_able_to(:manage, entity1) }
+    And { i.should be_able_to(:manage, entity2) }
   end
 
 
