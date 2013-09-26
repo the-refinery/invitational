ad16f7877512cdc1f4114592aa7b3856176f79ba
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-09-26 13:55:06 -0400

    Dont' need the roles to be symbols now

--- lib/generators/invitational/invitational_generator.rb
+++ lib/generators/invitational/invitational_generator.rb
@@ -1,10 +1,10 @@
 class InvitationalGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-  argument :roles, type: :array, default: [":none", ":user", ":admin"], banner: "List of Roles"
+  argument :roles, type: :array, default: ["none", "user", "admin"], banner: "List of Roles"
 
   def initializer_file
-    @role_list = roles.join(", ")
+    @role_list = roles.map{|role| ":" + role}.join(", ")
     template "initializer.rb", "config/initializers/invitational.rb"
   end
 
