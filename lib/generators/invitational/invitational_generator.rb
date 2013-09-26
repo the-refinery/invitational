22d4c99d4a10c80e0e9a679e7c7677fcf2bba8a8
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-09-26 15:12:32 -0400

    Add the Ability class

--- lib/generators/invitational/invitational_generator.rb
+++ lib/generators/invitational/invitational_generator.rb
@@ -1,6 +1,7 @@
 class InvitationalGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
+  #argument :identity_class, type: :string, default: "User", banner: "Class name of identity (aka User) model"
   argument :roles, type: :array, default: ["none", "user", "admin"], banner: "List of Roles"
 
   def initializer_file
@@ -8,7 +9,11 @@ class InvitationalGenerator < Rails::Generators::Base
     template "initializer.rb", "config/initializers/invitational.rb"
   end
 
+  def ability_model
+    template "ability.rb", "app/models/ability.rb"
+  end
+
   def install_migration
-    rake("invitational_engine:install:migrations")
+    #rake("invitational_engine:install:migrations")
   end
 end
