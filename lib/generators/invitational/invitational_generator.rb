b48839537afe8753085a9a3b8565bb6ff787adcd
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-09-27 13:06:55 -0400

    USAGE and banner text updates

--- lib/generators/invitational/invitational_generator.rb
+++ lib/generators/invitational/invitational_generator.rb
@@ -1,7 +1,7 @@
 class InvitationalGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-  argument :identity_class, type: :string, default: "User", banner: "Class name of identity (aka User) model"
+  argument :identity_class, type: :string, default: "User", banner: "Class name of identity model (e.g. User)"
   argument :roles, type: :array, default: ["none", "user", "admin"], banner: "List of Roles"
 
   def initializer_file
