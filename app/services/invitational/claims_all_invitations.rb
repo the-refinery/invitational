00403b3efde5bc3596a863b2eae0df01a4db2d9a
Dave Goerlich <dgoerlich@designinginteractive.com>
2014-03-01 10:27:47 -0500

    Bug fix: ClaimAllInvitations correctly claims invitations now

--- app/services/invitational/claims_all_invitations.rb
+++ app/services/invitational/claims_all_invitations.rb
@@ -4,9 +4,10 @@ module Invitational
     def self.for user
       pending_invitations = ::Invitation.pending_for(user.email)
 
-      pending_invitations.each do |invite|
-        invite.user = user
-        invite.save
+      pending_invitations.each do |invitation|
+        invitation.user = user
+        invitation.date_accepted = DateTime.now
+        invitation.save
       end
     end
   end
--- lib/invitational/version.rb
+++ lib/invitational/version.rb
@@ -1,3 +1,3 @@
 module Invitational
-  VERSION = "1.1.2"
+  VERSION = "1.1.3"
 end
